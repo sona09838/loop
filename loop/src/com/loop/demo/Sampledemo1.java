7909c8384417d5ca39af9ac82fd8319ffbbeeed5
sona <ssonalishinde1995.sgcs@gmail.com>
2020-08-26 19:57:39 +0530

    loop

---
+++ loop/src/com/loop/demo/Sampledemo1.java
@@ -0,0 +1,18 @@
+package com.loop.demo;
+
+public class Sampledemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{int i,j,count=0;
+		for(j=1;j<=7;j++)
+		{
+			for(i=1;i<=5;i++)
+				{count=count+1;
+				System.out.print(j+" ");	
+		}
+		System.out.println(" ");
+		}}
+	
+
+}}
